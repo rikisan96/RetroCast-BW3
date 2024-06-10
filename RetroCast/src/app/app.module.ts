66c58bd5f71431e4a7facb87642938a3d89f0858
rikisan96 <riccardocutruneo@gmail.com>
2024-06-10 13:15:01 +0200

    added todoList, some components

--- RetroCast/src/app/app.module.ts
+++ RetroCast/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './main-components/navbar/navbar.component';
+import { NavbarFooterComponent } from './main-components/navbar-footer/navbar-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarFooterComponent
   ],
   imports: [
     BrowserModule,
