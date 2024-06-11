85f41f8f03a01415195ab3d8ab19350123c6d506
Stefanospi <sspitaleri26@gmail.com>
2024-06-11 18:17:04 +0200

    push(da completare)

--- RetroCast/src/app/pages/arcade/arcade.component.ts
+++ RetroCast/src/app/pages/arcade/arcade.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-arcade',
   templateUrl: './arcade.component.html',
   styleUrl: './arcade.component.scss'
 })
-export class ArcadeComponent {
 
+@Pipe({
+  name: 'truncate'
+})
+
+export class ArcadeComponent implements PipeTransform {
+  PipeTransform {
+    transform(value: string, maxLength: number): string {
+      if (value.length > maxLength) {
+        return value.substring(0, maxLength) + '...';
+      }
+      return value;
+    }
+  }
 }
