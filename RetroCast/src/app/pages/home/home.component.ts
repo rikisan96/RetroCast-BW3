3e144334dff9724abe137db6514b484735cc551b
jasminemahfouz <jasmine12mahfouz@gmail.com>
2024-06-11 09:47:30 +0200

    modifica homepage

--- RetroCast/src/app/pages/home/home.component.ts
+++ RetroCast/src/app/pages/home/home.component.ts
@@ -1,21 +1,53 @@
-import { Component } from '@angular/core';
+// home.component.ts
+import { Component, OnInit } from '@angular/core';
 import { iGameList } from '../../Models/i-game-list';
 import { GamesService } from '../../Service/games.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  gameList: iGameList[]= [];
+export class HomeComponent implements OnInit {
+  gameList: iGameList[] = [];
+  filteredGameList: iGameList[] = [];
+  activeFilter: string | null = null;
 
-  constructor(private gameSvc:GamesService){}
+  constructor(private gameSvc: GamesService) {}
 
-  ngOnInit(){
-    this.gameSvc.getAllGames().subscribe((game) => {
-      this.gameList = game;
-      console.log(this.gameList);
-    })
+  ngOnInit() {
+    this.gameSvc.getAllGames().subscribe((games) => {
+      this.gameList = games;
+      this.filteredGameList = games;
+      console.log('Loaded games:', this.gameList);
+    });
+  }
+
+  filterByYear(startYear: number, endYear: number) {
+    this.filteredGameList = this.gameList.filter(game => game.year >= startYear && game.year <= endYear);
+    console.log(`Filtered games from ${startYear} to ${endYear}:`, this.filteredGameList);
+  }
+
+  filterByGenre(genre: string) {
+    this.filteredGameList = this.gameList.filter(game => game.genre === genre);
+    console.log(`Filtered games by genre (${genre}):`, this.filteredGameList);
+  }
+
+  filterByPlatform(platform: string) {
+    this.filteredGameList = this.gameList.filter(game => game.sysRequirement.includes(platform));
+    console.log(`Filtered games by platform (${platform}):`, this.filteredGameList);
+  }
+
+  showForm(filterType: string) {
+    this.activeFilter = filterType;
+  }
+
+  hideForm() {
+    this.activeFilter = null;
+  }
+
+  showAllGames() {
+    this.filteredGameList = this.gameList;
+    console.log('Showing all games:', this.filteredGameList);
   }
 }
