ac46c7d41f0ee0b832793348f460688727c4fae8
jasminemahfouz <jasmine12mahfouz@gmail.com>
2024-06-12 15:45:04 +0200

    testo card

--- RetroCast/src/app/pages/arcade/arcade.component.ts
+++ RetroCast/src/app/pages/arcade/arcade.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './arcade.component.scss'
 })
 export class ArcadeComponent {
+  readMore: { [key: string]: boolean } = {
+    pacman: false,
+    pingpong: false,
+    snake: false,
+    breakout: false
+  };
+
+  toggleReadMore(game: string) {
+    this.readMore[game] = !this.readMore[game];
+  }
 }
