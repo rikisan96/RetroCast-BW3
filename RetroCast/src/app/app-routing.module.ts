8c42f8fb50a4c18ff39e3f35cd04731ef216ea9c
FraIanna <francescoiannaccone9@gmail.com>
2024-06-12 09:27:58 +0200

    update home routing

--- RetroCast/src/app/app-routing.module.ts
+++ RetroCast/src/app/app-routing.module.ts
@@ -6,21 +6,27 @@ import { GuestGuard } from './auth/guest.guard';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
     canActivate: [GuestGuard],
     canActivateChild: [GuestGuard],
   },
   {
-    path:'login',
-    loadChildren:() => import('./auth/auth.module').then((m) => m.AuthModule),
-    canActivate: [GuestGuard],
-    canActivateChild: [GuestGuard],
-  },
-  { path: 'library', loadChildren: () => import('./pages/library/library.module').then(m => m.LibraryModule),
+    path: 'library',
+    loadChildren: () =>
+      import('./pages/library/library.module').then((m) => m.LibraryModule),
     canActivate: [AuthGuard],
-    canActivateChild: [AuthGuard]
-  }
+    canActivateChild: [AuthGuard],
+  },
 ];
 
 @NgModule({
