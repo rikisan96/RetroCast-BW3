c32de8bdd7993fd86a4a0372c9cb1a782e8dee30
jasminemahfouz <jasmine12mahfouz@gmail.com>
2024-06-12 11:51:21 +0200

    ..

--- RetroCast/src/app/app-routing.module.ts
+++ RetroCast/src/app/app-routing.module.ts
@@ -36,8 +36,6 @@ const routes: Routes = [
   },
   { path: 'pingpong', loadChildren: () => import('./pages/pingpong/pingpong.module').then(m => m.PingpongModule),canActivate: [AuthGuard],
     canActivateChild: [AuthGuard] },
-  { path: 'arcade', loadChildren: () => import('./pages/arcade/arcade.module').then(m => m.ArcadeModule),canActivate: [AuthGuard],
-    canActivateChild: [AuthGuard], },
   { path: 'pacman', loadChildren: () => import('./pages/pacman/pacman.module').then(m => m.PacmanModule),canActivate: [AuthGuard],
     canActivateChild: [AuthGuard], }
 ];
